91f54a85d3caf8256447c6821c788fb388ddf405
Chris Simmons <Chris.Simmons@quikrete.com>
2021-12-22 15:57:01 -0500

    end of section 2

---
+++ Persistence/Migrations/20211222202516_updateActivities.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class updateActivities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Categoru",
+                table: "Activities",
+                newName: "Category");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Activities",
+                newName: "Categoru");
+        }
+    }
+}
