704227321a6d6474f8cb7d67d455e22e9c6274a0
Chris Simmons <Chris.Simmons@quikrete.com>
2021-12-28 18:04:20 -0500

    bare bones crud operations

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -6,35 +6,52 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController:BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context) 
-        {
-            _context = context;
 
+
+        public ActivitiesController(IMediator mediator) 
+        {
+            
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return  await _context.Activities.OrderBy(item => item.Date).ToListAsync();
+            return  await Mediator.Send(new List.Query());
       
         }
 
         [HttpGet("{id}")]
+
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            var activity = await _context.Activities.FindAsync(id);
-            // if (activity == null)
-            // {
-            //     return NotFound();
-            // }
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
 
-            return activity;
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return  Ok(await Mediator.Send(new Create.Command {Activity = activity})); 
+            
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
 
     }
